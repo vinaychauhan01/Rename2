8bcdd29585d9e695590da3f8aafef98a0fe52335
vinay <147112817+vinaychauhan01@users.noreply.github.com>
2025-06-29 14:09:05 +0530

    Update utils.py

--- helper/utils.py
+++ helper/utils.py
@@ -35,8 +35,8 @@ async def progress_for_pyrogram(current, total, ud_type, message, start):
 
 def generate_progress_bar(percentage):
     return (
-                    "".join(["⬢" for _ in range(math.floor(percentage / 5))])Add commentMore actions
-                 "".join(["⬡" for _ in range(20 - math.floor(percentage / 5))])
+        "".join(["⬢" for _ in range(math.floor(percentage / 5))])
+        + "".join(["⬡" for _ in range(20 - math.floor(percentage / 5))])
     )
 
 
